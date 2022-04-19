22035a804c74e4ba0958d34436e33fc3535dd587
DanielFerreiraDev <ferreira-estudante@hotmail.com>
2022-04-19 12:00:03 -0300

    Tornando classe View como abstrata assim como o método template para que as classes filhas implementem

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,4 +1,4 @@
-export class View<T> {
+export abstract class View<T> { //Toda classe abstrata pode ter 0 ou mais métodos abstratos(um método que a classse pai não sabe como vai ser implementado)
 
     protected elemento: HTMLElement;
 
@@ -11,7 +11,5 @@ export class View<T> {
         this.elemento.innerHTML = template;
     }
 
-    template(model: T): string {
-        throw Error('Classe filha precisa implementar o método template!')
-    }
+    abstract template(model: T): string; //Não pode esquecer de implementar esse método nas classes filhas caso for instanciado.
 }
