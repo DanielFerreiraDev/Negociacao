1d131bd343aebe513741d27eeab7bd50c50fc6cf
DanielFerreiraDev <ferreira-estudante@hotmail.com>
2022-04-19 12:07:03 -0300

    Alterando tipo da classe do alerta

--- app/views/mensagem-view.ts
+++ app/views/mensagem-view.ts
@@ -4,7 +4,7 @@ export class MensagemView extends View<string>{
 
     template(model: string): string {
         return `
-            <p class="alert alert-info">${model}</p>
+            <p class="alert alert-success">${model}</p>
         `
     }
 
