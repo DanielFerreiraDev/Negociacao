262c7986e7ad0723b14671bd5652d181258ee8b5
DanielFerreiraDev <ferreira-estudante@hotmail.com>
2022-04-24 21:59:31 -0300

    Mudando tratamento do retorno null para os elementos de view e app.ts

--- app/views/view.ts
+++ app/views/view.ts
@@ -4,7 +4,12 @@ export abstract class View<T> { //Toda classe abstrata pode ter 0 ou mais métod
     private escapar = false;
 
     constructor(seletor: string, escapar?: boolean) {
-        this.elemento = document.querySelector(seletor) as HTMLInputElement;
+        const elemento = document.querySelector(seletor);
+        if (elemento) {
+            this.elemento = elemento as HTMLElement;
+        } else {
+            throw Error(`Seletor ${seletor} não existe no DOM. Verifique.`)
+        }
         if (escapar) {
             this.escapar = escapar;
         }
