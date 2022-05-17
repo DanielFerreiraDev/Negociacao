fa01294881b7bf7900ffc3315e57990436ea4852
DanielFerreiraDev <ferreira-estudante@hotmail.com>
2022-05-17 18:15:21 -0300

    Implementando decorator de logar-tempo, utilizando recursos do JS para solucionar caso

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -4,6 +4,17 @@ export function logarTempoDeExecucao() {
         propertyKey: string, //Nome do método que foi decorado
         descriptor: PropertyDescriptor //Sabe tudo sobre o método que queremos modificar, referência pro método original
     ) {
+
+        const metodoOriginal = descriptor.value;
+        
+        descriptor.value = function(...args: any[]) {
+            const t1 = performance.now();
+            const retorno = metodoOriginal.apply(this, args); //Chamar método original
+            const t2 = performance.now();
+            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1/1000)} segundos`);
+            retorno
+        };
+
         return descriptor;
     }
 }
